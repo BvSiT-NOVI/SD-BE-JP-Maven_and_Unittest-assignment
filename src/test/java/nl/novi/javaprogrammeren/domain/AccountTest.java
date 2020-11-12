908a1766bae39d2cce9f4d111fe8455e26dbd947
Nick Stuivenberg <n.stuivenberg@novi.nl>
2020-11-12 11:04:27 +0100

    Made it possible to run Unit-tests with Maven.
    Added plugin in pom.xml. Changed artifact id to correct id.
    
    Added AccountTest to test if testing works.
    
    How to make a Maven project from a non-Maven project in IntelliJ:
    https://www.jetbrains.com/help/idea/convert-a-regular-project-into-a-maven-project.html

--- src/test/java/nl/novi/javaprogrammeren/domain/AccountTest.java
+++ src/test/java/nl/novi/javaprogrammeren/domain/AccountTest.java
@@ -1,4 +1,21 @@
 package nl.novi.javaprogrammeren.domain;
 
-public class AccountTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class AccountTest {
+
+    @Test
+    void depositingNegativeNumberShouldReturnFalse() {
+
+        //Arrange
+        Account account = new Account("Nick", 12, 400);
+
+        //Act
+        boolean isDeposited = account.deposit(-12);
+
+        //Assert
+        assertFalse(isDeposited);
+    }
 }
